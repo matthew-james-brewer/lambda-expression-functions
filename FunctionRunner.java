1d5820b63985754a3d0b12078281f4539b40821a
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-20 09:19:10 -0500

    Update FunctionRunner.java with JavaDoc

--- FunctionRunner.java
+++ FunctionRunner.java
@@ -1,10 +1,21 @@
-import java.util.HashMap;
-
+/**
+ * Executes a function with the specified arguments and return type.
+ *
+ * @param <R> the return type of the function
+ */
 public class FunctionRunner<R> {
     private Function<R> in;
     private HashMap<String, Class<?>> args;
 
-    public FunctionRunner(Class<?>[] c, String[] s, Function<R> f) {
+    /**
+     * Constructs a FunctionRunner instance with the specified arguments.
+     *
+     * @param c an array of argument types
+     * @param s an array of argument names
+     * @param f the function to execute
+     * @throws IllegalArgumentException if the lengths of the argument arrays do not match
+     */
+    public FunctionRunner(Class<?>[] c, String[] s, Function<R> f) throws IllegalArgumentException {
         if (c.length != s.length) throw new IllegalArgumentException("Arrays not equal lengths");
         args = new HashMap<>();
         for (int i = 0; i < s.length; i++) {
@@ -15,6 +26,13 @@ public class FunctionRunner<R> {
         in = f;
     }
 
+    /**
+     * Executes the function with the provided arguments.
+     *
+     * @param o the arguments to pass to the function
+     * @return the result of the function execution
+     * @throws IllegalArgumentException if the argument types do not match the expected types
+     */
     public R exec(Object... o) {
         HashMap<String, Object> h = new HashMap<>();
         int i = 0;
