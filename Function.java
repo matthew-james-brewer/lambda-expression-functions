7974b1738249fe146a918c48e7c10ddc4257d811
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-20 09:18:17 -0500

    Update Function.java with JavaDoc

--- Function.java
+++ Function.java
@@ -1,5 +1,15 @@
-import java.util.HashMap;
-
+/**
+ * Represents a function with arguments and a return type.
+ * Implement this interface to define custom functions.
+ *
+ * @param <R> the return type of the function
+ */
 public interface Function<R> {
+    /**
+     * Executes the function with the given arguments.
+     *
+     * @param hash a HashMap containing the arguments as key-value pairs
+     * @return the result of the function execution
+     */
     R exec(HashMap<String, Object> hash);
 }
