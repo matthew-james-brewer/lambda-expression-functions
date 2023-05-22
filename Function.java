cf63894cc26e7f5bc9526f576a49edc9fdef31da
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-22 07:00:43 -0500

    Update Function.java

--- Function.java
+++ Function.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
  * Represents a function (a lambda expression) with arguments and a return type.
  * Put a lambda expression in place of this interface to define custom functions.
  *
+ * @author Matttew Brewer: mjbrewer17@gmail.com
  * @param <R> the return type of the function
  * @see FunctionRunner
  */
