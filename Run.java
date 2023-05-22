694959312c26c2c52a2d4da4366a9f0e3f07f06e
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-22 07:37:20 -0500

    Update Run.java with new class

--- Run.java
+++ Run.java
@@ -11,5 +11,29 @@ public class Run {
         );
       //use [var name].exec([args]) to run it and return the result
       System.out.println(basic.exec(2));
+	  System.out.println(ImplementExample.getFr.exec(10, "banana"));
     }
+}
+//implement it to create functions that need to be put in smaller chunks
+class ImplementExample implements Function<String> {
+	//add fields if you like
+	private static final int max = new Integer(15);
+	private static final int min = new Integer(-10);
+	//make it easy to run with a FunctionRunner inside
+	private static FunctionRunner<Integer> fr = new FunctionRunner<>(
+		new Class<?>[]{Integer.class,String.class},
+		new String[]{"number","text"},
+		this);
+	public static FunctionRunner<String> getFr(){return fr;}
+	//here is the main method:
+	public static String exec(HashMap<String, Object> hash) {
+		return implement("Why did you say \""+hash.get("text")+"\"?",hash.get("number"));
+	}
+	//add as many functions as you want
+	public static String implement(Integer i, String s) throws IndexOutOfBoundsException {
+		if(i.intValue() > max || i.intValue() < min) {
+			throw new IndexOutOfBoundsException("out of range");
+		}
+		return s+i.toString();
+	}
 }
