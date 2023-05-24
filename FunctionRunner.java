b01d6381a9f30eedd1bd44364e31e9d6aa0a0303
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-24 07:45:57 -0500

    Update FunctionRunner.java

--- FunctionRunner.java
+++ FunctionRunner.java
@@ -1,12 +1,10 @@
 import java.util.HashMap;
-/** executes a {@link Function} with the specified arguments and return type.
- *
- * @author Matttew Brewer: mjbrewer17@gmail.com
- * @param <R> the return type of the function
- * @see {@link Function}
+
+/**
+ * Executes a {@link Function} with the specified arguments and return type.
  */
-public class FunctionRunner<R> {
-    private Function<R> in;
+public class FunctionRunner {
+    private Function<?>[] functions;
     private HashMap<String, Class<?>> args;
 
     /**
@@ -17,40 +15,74 @@ public class FunctionRunner<R> {
      * @param f the function to execute
      * @throws IllegalArgumentException if the lengths of the argument arrays do not match
      */
-    public FunctionRunner(Class<?>[] c, String[] s, Function<R> f) throws IllegalArgumentException {
-        if (c.length != s.length) throw new IllegalArgumentException("Types and identifier pairs incomplete: "+c.length+" types and "+s.length+" identifiers");
-        args = new HashMap<>();
-        for (int i = 0; i < s.length; i++) {
-            String key = s[i];
-            Class<?> value = c[i];
-            args.put(key, value);
+    public FunctionRunner(Class<?>[][] c, String[][] s, Function<?>[] f) throws IllegalArgumentException {
+        if (c.length != s.length || c.length != f.length) {
+            throw new IllegalArgumentException("Number of argument types, argument names, and functions do not match");
         }
-        in = f;
+
+        args = new HashMap<>();
+        for (int i = 0; i < c.length; i++) {
+            Class<?>[] argTypes = c[i];
+            String[] argNames = s[i];
+            Function<?> function = f[i];
+
+            if (argTypes.length != argNames.length) {
+                throw new IllegalArgumentException("Types and identifier pairs incomplete for function: " + i);
+            }
+
+            for (int j = 0; j < argTypes.length; j++) {
+                String argName = argNames[j];
+                Class<?> argType = argTypes[j];
+                args.put(argName, argType);
+            }
+        }
+
+        functions = f;
     }
 
     /**
      * Executes the function with the provided arguments.
      *
      * @param o the arguments to pass to the function
-     * @return the result of the function execution converted to R
-     * @throws IllegalArgumentException if the arguments' length do not match the expected arguments' length or argument types do not match the expected types
-     * @see Function#exec(HashMap)
+     * @return the result of the function execution
+     * @throws IllegalArgumentException if the arguments' length does not match the expected arguments' length
+     *                                  or if the argument types do not match the expected types
      */
-    public R exec(Object... o) throws IllegalArgumentException {
+    public Object exec(Object... o) throws IllegalArgumentException {
         if (o.length != args.size()) {
             throw new IllegalArgumentException("Formal and actual argument lists differ in length. Expected: " + args.size() + ", Actual: " + o.length);
         }
-        HashMap<String, Object> h = new HashMap<>();
+
+        HashMap<String, Object> arguments = new HashMap<>();
         int i = 0;
-        for (String key : args.keySet()) {
-            Class<?> expectedType = args.get(key);
+        for (String argName : args.keySet()) {
+            Class<?> expectedType = args.get(argName);
             Object value = o[i];
-            if (!expectedType.isInstance(value)) {
-                throw new IllegalArgumentException("Incorrect type of argument value for '" + key + "'. Expected: " + expectedType.getSimpleName() + ", Actual: " + value.getClass().getSimpleName());
+
+            if (!isArgumentTypeCompatible(expectedType, value)) {
+                throw new IllegalArgumentException("Incorrect type of argument value for '" + argName + "'. Expected: " + expectedType.getSimpleName() + ", Actual: " + value.getClass().getSimpleName());
             }
-            h.put(key, value);
+
+            arguments.put(argName, value);
             i++;
         }
-        return in.exec(h);
+
+        return executeFunction(arguments);
+    }
+
+    private boolean isArgumentTypeCompatible(Class<?> expectedType, Object value) {
+        return expectedType.isInstance(value);
+    }
+
+    private Object executeFunction(HashMap<String, Object> arguments) {
+        for (Function<?> function : functions) {
+            try {
+                return function.exec(arguments);
+            } catch (IllegalArgumentException e) {
+                // Ignore the exception and continue to the next function
+            }
+        }
+
+        throw new IllegalArgumentException("No matching function found");
     }
 }
