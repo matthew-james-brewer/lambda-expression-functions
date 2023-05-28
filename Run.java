fb734db7d1b8a81733301f45084ecec1590cd4c4
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2023-05-28 08:22:01 -0500

    Update Run.java

--- Run.java
+++ Run.java
@@ -1,15 +1,18 @@
 import java.util.HashMap;
 public class Run {
     public static void main(String[] args) {
-        FunctionRunner<String> basic /*replace 'String' with return type and 'basic' with name for function*/ = new FunctionRunner<>(
-                new Class<?>[]{/*list of argument types(such as*/Integer.class/*)*/},
-                new String[]{/*list of names for arguments(such as*/"num"/*)*/},
-                (values) -> {
+        Function<String> link = //replace 'String' with the return type
+            (values) -> {
                     //function using '([type])values.get([arg name])' to get arguments, such as
                     Integer num = (Integer)values.get("num");
                     return "Good job in the race! You got #"+num.toString()+" place! Bye!"; //replace with returned object
-                }
-        );
+            };
+        FunctionRunner basic /*replace'basic' with name for function*/ = new FunctionRunner(
+                new Class<?>[][]{/*list of argument types for each function in overload (such as*/{Integer.class}/*)*/},
+                new String[][]{/*list of names for arguments in each function in overload(such as*/{"num"}/*)*/},
+                new Function<?>[]{
+                    link // use variables to show where the function is
+                });
       //use [var name].exec([args]) to run it and return the result
       System.out.println(basic.exec(2));
       System.out.println((new ImplementExample()).getFr().exec(10, "banana"));
@@ -57,14 +60,14 @@ class ImplementExample implements Function<String> {
 	private static final int max = 15;
 	private static final int min = -10;
 	//make it easy to run with a FunctionRunner inside
-	private FunctionRunner<String> fr = new FunctionRunner<>(
-		new Class<?>[]{Integer.class,String.class},
-		new String[]{"number","text"},
-		this);
-	public FunctionRunner<String> getFr(){return fr;}
+	private FunctionRunner fr = new FunctionRunner(
+		new Class<?>[][]{{Integer.class, String.class}},
+		new String[][]{{"number","text"}},
+		new Function<?>[]{this});
+	public FunctionRunner getFr(){return fr;}
 	//Here is the main method:
 	public String exec(HashMap<String, Object> hash) {
-		return implement("Why did you say \""+(String)hash.get("text")+"\"?\nexit code:"(Integer)hash.get("number"));
+		return implement("Why did you say \""+(String)hash.get("text")+"\"?\nexit code:",(Integer)hash.get("number"));
 	}
 	//add as many functions as you want
 	public static String implement(String s, Integer i) throws IndexOutOfBoundsException {
